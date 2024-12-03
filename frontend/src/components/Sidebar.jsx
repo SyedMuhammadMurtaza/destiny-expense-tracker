fd75956a18b7590970196140e2d8a3fbac665f06
SyedMuhammadMurtaza <smmhj19@gmail.com>
2024-12-03 22:10:17 +0500

    Sidebar Logout Position Fixed

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -89,7 +89,8 @@ const Sidebar = () => {
         </nav>
 
         {/* Logout Button */}
-        <div className="absolute bottom-0 left-0 w-full p-4">
+        {/* <div className="absolute bottom-0 left-0 w-full p-4"> */}
+        <div className="absolute left-0 w-full p-4">
           <button
             onClick={handleLogout}
             className="w-full py-2 px-4 bg-red-600 text-white rounded-md hover:bg-red-700"
